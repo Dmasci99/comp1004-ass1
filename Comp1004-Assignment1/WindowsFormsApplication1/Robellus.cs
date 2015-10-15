1f1ad1da0174d04f7fb71527244136cca3591f20
Alex Friesen <200302342@student.georgianc.on.ca>
2015-10-15 14:47:49 -0400

    Second Commit
    Gave the buttons the ability to click.

--- Comp1004-Assignment1/WindowsFormsApplication1/Robellus.cs
+++ Comp1004-Assignment1/WindowsFormsApplication1/Robellus.cs
@@ -12,23 +12,74 @@ namespace WindowsFormsApplication1
 {
     public partial class RobellusForm : Form
     {
+        private decimal basePriceDecimal = 0, accessoryPriceDecimal = 0, tradeInDecimal = 0, subTotalDecimal = 0,
+            taxAmountDecimal = 0, totalDecimal = 0;
+        private const decimal TAX_DECIMAL = 0.13m, MONEY_FORMAT_DECIMAL = 0;
+
+
         public RobellusForm()
         {
             InitializeComponent();
+            
         }
         private void RobellusForm_Load(object sender, EventArgs e)
         {
+            clearForm();
+        }
+
+        private void iPhone6RadioButton_CheckedChanged(object sender, System.EventArgs e)
+        {
 
         }
 
-        private void exitButton_Click(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, System.EventArgs e)
         {
-            Close();
+
         }
 
-        private void calculateButton_Click(object sender, EventArgs e)
+        private void calculateButton_Click(object sender, System.EventArgs e)
         {
-            
+
+        }
+
+        private void clearButton_Click(object sender, System.EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void exitButton_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void leatherCaseCheckBox_CheckedChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void screenProtectorCheckBox_CheckedChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void clearForm()
+        {
+            iPhone6RadioButton.Checked = false;
+            iPhone6SRadioButton.Checked = false;
+            spaceGreyRadioButton.Checked = false;
+            silverRadioButton.Checked = false;
+            goldRadioButton.Checked = false;
+            capacity64RadioButton.Checked = false;
+            capacity16RadioButton.Checked = false;
+            capacity128RadioButton.Checked = false;
+            appleCareCheckBox.Checked = false;
+            leatherCaseCheckBox.Checked = false;
+            screenProtectorCheckBox.Checked = false;
+            basePriceTextBox.Text = MONEY_FORMAT_DECIMAL.ToString("c");
+            accessoriesTextBox.Text = MONEY_FORMAT_DECIMAL.ToString("c");
+            tradeInTextBox.Text = MONEY_FORMAT_DECIMAL.ToString("c");
+            taxTextBox.Text = MONEY_FORMAT_DECIMAL.ToString("c");
+            totalTextBox.Text = MONEY_FORMAT_DECIMAL.ToString("c");
         }
     }
 }
